9cb27c2ddebae198d274c7cf530c411a04dd7e10
Murad <=>
2021-02-12 13:43:11 +0400

    İndex.html-də about section classları dəyişdirildi.Onlara uyğun css-lər düzəldildi

--- design/js/script.js
+++ design/js/script.js
@@ -9,6 +9,25 @@ medal.addEventListener('click' , function() {
     if(document.getElementById('active-1').style.display = 'none'){
         document.getElementById('active').style.display = 'none'
         document.getElementById('active-1').style.display = 'block'
+
+        main.style.color = '#828282'
+        main.style.backgroundColor = '#fff'
+        main.style.borderRadius = 'none'
+        main.style.fontWeight = '700'
+
+        medal.style.backgroundColor = '#02d464'
+        medal.style.color = '#fff'
+        medal.style.borderRadius = '30px'
+        medal.style.fontWeight = '100'
+        medal.style.padding = '5px 15px'
+        medal.style.width = '60%'
+        medal.style.marginLeft = '5px'
+
+        
+
+        
+
+       
     }
 })
 
@@ -21,6 +40,21 @@ main.addEventListener('click' , function() {
     if(document.getElementById('active').style.display = 'none'){
         document.getElementById('active-1').style.display = 'none'
         document.getElementById('active').style.display = 'block'
+
+
+        medal.style.color = '#828282'
+        medal.style.backgroundColor = '#fff'
+        medal.style.borderRadius = 'none'
+        medal.style.fontWeight = '700'
+
+        main.style.backgroundColor = '#02d464'
+        main.style.color = '#fff'
+        main.style.borderRadius = '30px'
+        main.style.fontWeight = '100'
+        main.style.padding = '5px 15px'
+
+       
+        
     }
 })
 
