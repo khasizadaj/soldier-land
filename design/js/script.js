05fcb20c56c61931298a48d1b4f5c6b5f112b358
Murad <=>
2021-02-14 21:20:39 +0400

    Javascript kodları yeniləndi

--- design/js/script.js
+++ design/js/script.js
@@ -1,31 +1,39 @@
-var medal = document.getElementById('medal')
+var medal = document.getElementById('medal-button')
 var main = document.getElementById('main-info')
 
-document.getElementById('active-1').style.display = 'none'
+
 
 
 
 medal.addEventListener('click' , function() {
-    if(document.getElementById('active-1').style.display = 'none'){
+
+    
+
+
+
+    if(document.getElementById('medal-data').style.display = 'none'){
         document.getElementById('active').style.display = 'none'
-        document.getElementById('active-1').style.display = 'block'
+        document.getElementById('medal-data').style.display = 'block'
 
-        main.style.color = '#828282'
-        main.style.backgroundColor = '#fff'
-        main.style.borderRadius = 'none'
-        main.style.fontWeight = '700'
+        medal.classList.remove('medal-js-1')
+        main.classList.remove('main-js-1')
 
-        medal.style.backgroundColor = '#02d464'
-        medal.style.color = '#fff'
-        medal.style.borderRadius = '30px'
-        medal.style.fontWeight = '100'
-        medal.style.padding = '5px 15px'
-        medal.style.width = '60%'
-        medal.style.marginLeft = '5px'
 
-        
+        medal.classList.add('medal-js')
+        main.classList.add('main-js')
 
-        
+        // main.style.color = '#828282'
+        // main.style.backgroundColor = '#fff'
+        // main.style.borderRadius = 'none'
+        // main.style.fontWeight = '700'
+
+        // medal.style.backgroundColor = '#02d464'
+        // medal.style.color = '#fff'
+        // medal.style.borderRadius = '30px'
+        // medal.style.fontWeight = '100'
+        // medal.style.padding = '5px 15px'
+        // medal.style.width = '60%'
+        // medal.style.marginLeft = '5px'
 
        
     }
@@ -38,20 +46,29 @@ medal.addEventListener('click' , function() {
 
 main.addEventListener('click' , function() {
     if(document.getElementById('active').style.display = 'none'){
-        document.getElementById('active-1').style.display = 'none'
+        document.getElementById('medal-data').style.display = 'none'
         document.getElementById('active').style.display = 'block'
 
+        medal.classList.remove('medal-js')
+        main.classList.remove('main-js')
 
-        medal.style.color = '#828282'
-        medal.style.backgroundColor = '#fff'
-        medal.style.borderRadius = 'none'
-        medal.style.fontWeight = '700'
 
-        main.style.backgroundColor = '#02d464'
-        main.style.color = '#fff'
-        main.style.borderRadius = '30px'
-        main.style.fontWeight = '100'
-        main.style.padding = '5px 15px'
+        medal.classList.add('medal-js-1')
+        main.classList.add('main-js-1')
+
+
+        // medal.style.color = '#828282'
+        // medal.style.backgroundColor = '#fff'
+        // medal.style.borderRadius = 'none'
+        // medal.style.fontWeight = '700'
+
+        // main.style.backgroundColor = '#02d464'
+        // main.style.color = '#fff'
+        // main.style.borderRadius = '30px'
+        // main.style.fontWeight = '100'
+        // main.style.padding = '5px 15px'
+
+        
 
        
         
