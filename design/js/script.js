0d49a75e37743f781f3cbf575309a3fb4c055a59
Murad <muzaffarov357@gmail.com>
2021-03-04 00:10:00 +0400

    Profile Form hazırlandı

--- design/js/script.js
+++ design/js/script.js
@@ -1,5 +1,5 @@
 
-// version 1 - a bit advanced - not the best probably
+
 
 const tabs = document.querySelectorAll('.wrapper-tab')
 const data = document.querySelectorAll('.about-section-data')
@@ -24,91 +24,17 @@ tabs.forEach((tab, index) => {
             }
         })
     })
-    
-    // version 2 - based on your idea - not bad probably not the best too
-    // both works fine 
-    
-// const medalBtn = document.getElementById('medal-button')
-// const mainBtn = document.getElementById('main-button')
-// const medalData = document.getElementById('medal-data')
-// const mainData = document.getElementById('main-data')
-
-// medalBtn.addEventListener('click', function (e) {
-//     let classes = e.target.classList;
-
-//     if (!classes.contains('active')) {
-//         medalBtn.classList.add('active');
-//         medalData.classList.add('active');
-
-//         mainBtn.classList.remove('active');
-//         mainData.classList.remove('active');
-//     }
-// });
-
-// mainBtn.addEventListener('click', function (e) {
-//     let classes = e.target.classList;
-
-//     if (!classes.contains('active')) {
-//         mainBtn.classList.add('active');
-//         mainData.classList.add('active');
-
-//         medalBtn.classList.remove('active');
-//         medalData.classList.remove('active');
-//     }
-// });
 
 
-// medalBtn.addEventListener('click', function () {
 
-//     if (document.getElementById('medal-data').style.display == 'none') {
-//         document.getElementById('active').style.display = 'none'
-//         document.getElementById('medal-data').style.display = 'block'
 
-//         medal.classList.remove('medal-js-1')
-//         main.classList.remove('main-js-1')
 
-//         medal.classList.add('medal-js')
-//         main.classList.add('main-js')
+// Add Story
 
-//         // main.style.color = '#828282'
-//         // main.style.backgroundColor = '#fff'
-//         // main.style.borderRadius = 'none'
-//         // main.style.fontWeight = '700'
+// const addStory = document.querySelector('.actions-span')
 
-//         // medal.style.backgroundColor = '#02d464'
-//         // medal.style.color = '#fff'
-//         // medal.style.borderRadius = '30px'
-//         // medal.style.fontWeight = '100'
-//         // medal.style.padding = '5px 15px'
-//         // medal.style.width = '60%'
-//         // medal.style.marginLeft = '5px'
-//     }
+// addStory.addEventListener('click' , function(){
+//         document.querySelector('.empty-stories').style.display = 'none'
+//         document.querySelector('.story-form').style.display = 'block'
 // })
-
-// // Main info
-
-// mainBtn.addEventListener('click', function () {
-//     if (document.getElementById('active').style.display == 'none') {
-//         document.getElementById('medal-data').style.display = 'none'
-//         document.getElementById('active').style.display = 'block'
-
-//         medal.classList.remove('medal-js')
-//         main.classList.remove('main-js')
-
-//         medal.classList.add('medal-js-1')
-//         main.classList.add('main-js-1')
-
-
-//         // medal.style.color = '#828282'
-//         // medal.style.backgroundColor = '#fff'
-//         // medal.style.borderRadius = 'none'
-//         // medal.style.fontWeight = '700'
-
-//         // main.style.backgroundColor = '#02d464'
-//         // main.style.color = '#fff'
-//         // main.style.borderRadius = '30px'
-//         // main.style.fontWeight = '100'
-//         // main.style.padding = '5px 15px'
-//     }
-// })
-
+   
